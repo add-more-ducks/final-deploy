bab304f25b80fb0514cf18473da7a24acb46176c
add-more-ducks <thomas.van.genderen@student.uva.nl>
2021-04-29 18:09:04 +0200

    Experimentation with the french solution and json

--- PROCESS.md
+++ PROCESS.md
@@ -36,3 +36,14 @@ Today I got the site running, but have not implemented any functions yet.
 
 # 23 April
 I have dicided to make hosting the .csv locally the first priority. Afterwards i might try to run timelineJS completely local. 
+
+# 23 April
+I run into the problem of having Flask communicate with the .csv and .js files.
+In order to use the .js files I need to keep them in a /static folder. To communicate with the .csv, it needs to stay there as well. I now see the difficulty of working with .csv as this probably means it will be hard to edit later. Should I stay with the external google sheets?
+Maybe I should just keep the events in my SQL database instead. Will I be able to use them instead?. 
+
+I have a hard time deciding what to do. Hopefully i can discuss it with other students.
+
+Should i work via google forms?
+
+Now I got embeded to work, but it can't find the json
---
+++ app/static/js/timeline-local.js
@@ -0,0 +1,50 @@
+const options = {
+  language: 'en',
+};
+//
+// load the data from a local csv
+//
+
+const csvData = {
+  events: [],
+};
+
+d3.csv('/timeline-data.csv', (error, data) => {
+  data.forEach((itemData) => {
+        // reference for json format: https://timeline.knightlab.com/docs/json-format.html
+        // inspired from https://github.com/NUKnightLab/TimelineJS3/blob/master/source/js/core/TL.ConfigFactory.js
+    // console.log(itemData);
+    const event = {
+      start_date: TL.Date.parseDate(itemData.date),
+      media:
+      {
+        caption: itemData.mediaCaption || '',
+        credit: itemData.mediaCredit || '',
+        url: itemData.mediaUrl || '',
+        thumbnail: itemData.mediaThumbnail || '',
+      },
+      text:
+      {
+        headline: itemData.titre || '',
+        text: itemData.texte || '',
+      },
+      background: {
+        url: itemData.backgroundUrl || '',
+        color: itemData.backgroundColor || '',
+      },
+    };
+
+    if (itemData.type === 'title') {
+      if (!csvData.title) {
+        csvData.title = event;
+      } else {
+        console.log('Multiple title slides detected.');
+        csvData.events.push(event);
+      }
+    } else {
+      csvData.events.push(event);
+    }
+  });
+  // console.log(csvData);
+  const timeline = new TL.Timeline('timeline', csvData, options);
+});
